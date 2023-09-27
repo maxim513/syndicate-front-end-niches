9738ba19c66fbb4c376bec85ff6b40008caa784c
Max <143605086+maxim513@users.noreply.github.com>
2023-09-27 15:32:52 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+    }
+public class RandomDataGenerator {
+
+        for (int item : data) {
+        }
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        }
+        for (int i = 0; i < 10; i++) {
+}
