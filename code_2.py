2ad0e47ab84e301575b81bd4770eb23c5f6f9d82
Max <143605086+maxim513@users.noreply.github.com>
2023-10-01 10:13:03 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,62 @@
+}
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+    });
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+function generateRandomData() {
+
+    return data;
+
+    }
+main();
+    }
+    const data = generateRandomData();
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    const data = [];
+
+}
+    data.forEach(item => {
+
+}
+        console.log("Random Number: " + item);
+main();
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+function main() {
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+
+    return data
+def main():
+        print(f"Random Number: {item}")
+    for item in data:
+
+
+
+import random
+    main()
+if __name__ == "__main__":    }
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+main();
+
+
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    const data = generateRandomData();
+}
+    return data;
+}
+function main() {
+    });
+
+    const data = [];
